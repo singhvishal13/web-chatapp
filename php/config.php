ffa16d15690dcef368ab9820b47cf37ade10946b
misc <misc@gmail.com>
2021-12-19 02:12:44 +0530

    third commit

--- php/config.php
+++ php/config.php
@@ -1,8 +1,9 @@
 <?php
-  $hostname = "localhost";
-  $username = "postgres";
-  $password = "admin";
-  $dbname = "chatapp";
+  
+  $hostname = "ec2-107-21-211-154.compute-1.amazonaws.com";
+  $username = "vzypvgtcwroumt";
+  $password = "ab982df63cdc7de78a365153fc2ae1cd0f96d463d0da6bfcfef81ec6c6da554b";
+  $dbname = "d5j3fu26r6bg04";
 
   $conn = pg_connect("host=$hostname user=$username password=$password dbname=$dbname")
           or die("Could not connect");
