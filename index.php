70093af0da2a276c80f3001ae4644b5c1de86320
misc <misc@gmail.com>
2021-12-18 21:42:32 +0530

    second commit

--- index.php
+++ index.php
@@ -14,25 +14,20 @@
         <div class="error-text"></div>
         <div class="name-details">
           <div class="field input">
-            <!-- <label>First Name</label> -->
             <input type="text" name="fname" placeholder="First name" required>
           </div>
           <div class="field input">
-            <!-- <label>Last Name</label> -->
             <input type="text" name="lname" placeholder="Last name" required>
           </div>
         </div>
         <div class="field input">
-          <!-- <label>Email Address</label> -->
           <input type="text" name="email" placeholder="Enter your email" required>
         </div>
         <div class="field input">
-          <!-- <label>Password</label> -->
           <input type="password" name="password" placeholder="Enter new password" required>
           <i class="fas fa-eye"></i>
         </div>
         <div class="field image">
-          <!-- <label>Select Image</label> -->
           <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg,image/x-PNG,image/GIF,image/JPEG,image/JPG" required>
         </div>
         <div class="field button">
